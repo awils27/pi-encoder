e2fcd205f386911bc6084932be2243d08a6fdacb
Aiden <68633820+awils27@users.noreply.github.com>
2023-01-11 23:21:21 +1000

    changed Oled resolution

--- OLed.py
+++ OLed.py
@@ -8,4 +8,4 @@ reset_pin = digitalio.DigitalInOut(board.D4) # any pin!
 cs_pin = digitalio.DigitalInOut(board.D5)    # any pin!
 dc_pin = digitalio.DigitalInOut(board.D6)    # any pin!
 
-oled = adafruit_ssd1306.SSD1306_SPI(128, 32, spi, dc_pin, reset_pin, cs_pin)
+oled = adafruit_ssd1306.SSD1306_SPI(128, 64, spi, dc_pin, reset_pin, cs_pin)
