21ffe60fc35e028ae75b5bb44912ddb9b0c35e27
Aiden <68633820+awils27@users.noreply.github.com>
2023-01-11 23:23:36 +1000

    Update OLed.py

--- OLed.py
+++ OLed.py
@@ -9,3 +9,7 @@ cs_pin = digitalio.DigitalInOut(board.D5)    # any pin!
 dc_pin = digitalio.DigitalInOut(board.D6)    # any pin!
 
 oled = adafruit_ssd1306.SSD1306_SPI(128, 64, spi, dc_pin, reset_pin, cs_pin)
+
+
+oled.fill(1)
+oled.show()
