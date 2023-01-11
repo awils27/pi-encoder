ec7d84a03a6cc8fb286fbeb6ce1218e6698709f0
Aiden <68633820+awils27@users.noreply.github.com>
2023-01-11 21:35:34 +1000

    Enable Clean exit

--- Main.py
+++ Main.py
@@ -2,6 +2,9 @@ from threading import Thread
 import Button
 import Encoder
 import DataSending
+import atexit
+from RPi import GPIO
+
 
 Button_GPIO = 16
 EncoderA = 21
@@ -11,14 +14,15 @@ UDP_PORT = 4000
 
 
 
-
+def ExitHandler():
+    GPIO.cleanup()
 
 def button_pressed_callback(channel):
     Encoder.counter = 0
     print("Lens Value Reset")
 
 
-
+atexit.register(ExitHandler)
 
 Encoder.SetupEncoders(EncoderA, EncoderB)
 T1Encoders = Thread(target = Encoder.EncoderThread.ReadEncoderValues, args = (EncoderA, EncoderB))
@@ -30,3 +34,5 @@ T1Encoders.start()
 T2Data.start()
 
 Button.SetupButton(Button_GPIO, button_pressed_callback)
+
+
