a64ed9ac438cfc1961ae4789d09f7a64d9d125f3
Aiden <68633820+awils27@users.noreply.github.com>
2023-01-11 22:46:59 +1000

    Update OLed.py

--- OLed.py
+++ OLed.py
@@ -1,222 +1,74 @@
-from RPi import GPIO
-import framebuf
-import time
-import spidev
+# SPDX-FileCopyrightText: 2021 ladyada for Adafruit Industries
+# SPDX-License-Identifier: MIT
 
-bus = 0
-device = 1
-spi = spidev.SpiDev()
-spi.open(bus, device)
+"""
+This demo will fill the screen with white, draw a black box on top
+and then print Hello World! in the center of the display
 
-spi.max_speed_hz = 500000
-spi.mode = 0
+This example is for use on (Linux) computers that are using CPython with
+Adafruit Blinka to support CircuitPython libraries. CircuitPython does
+not support PIL/pillow (python imaging library)!
+"""
 
-DC = 17
-RST = 22
-MOSI = 10
-SCK = 11
-CS = 27
+import board
+import digitalio
+from PIL import Image, ImageDraw, ImageFont
+import adafruit_ssd1306
 
-GPMOSI = GPIO.setup(MOSI, GPIO.IN)
-GPSCK = GPIO.setup(SCK, GPIO.IN)
+# Define the Reset Pin
+oled_reset = digitalio.DigitalInOut(board.D4)
 
-class OLED_1inch3(framebuf.FrameBuffer):
-    def __init__(self):
-        self.width = 128
-        self.height = 64
-        
-        self.cs =  GPIO.setup(CS, GPIO.OUT)
-        self.rst = GPIO.setup(RST, GPIO.OUT)
-        
-        self.cs(1)
-        self.dc = GPIO.setup(DC, GPIO.OUT)
-        self.dc(1)
-        self.buffer = bytearray(self.height * self.width // 8)
-        super().__init__(self.buffer, self.width, self.height, framebuf.MONO_HMSB)
-        self.init_display()
-        
-        self.white =   0xffff
-        self.balck =   0x0000
-        
-    def write_cmd(self, cmd):
-        self.cs(1)
-        self.dc(0)
-        self.cs(0)
-        self.spi.write(bytearray([cmd]))
-        self.cs(1)
+# Change these
+# to the right size for your display!
+WIDTH = 128
+HEIGHT = 64  # Change to 64 if needed
+BORDER = 5
 
-    def write_data(self, buf):
-        self.cs(1)
-        self.dc(1)
-        self.cs(0)
-        self.spi.write(bytearray([buf]))
-        self.cs(1)
+# Use for I2C.
+#i2c = board.I2C()  # uses board.SCL and board.SDA
+# i2c = board.STEMMA_I2C()  # For using the built-in STEMMA QT connector on a microcontroller
+#oled = adafruit_ssd1306.SSD1306_I2C(WIDTH, HEIGHT, i2c, addr=0x3C, reset=oled_reset)
 
-    def init_display(self):
-        """Initialize dispaly"""  
-        self.rst(1)
-        time.sleep(0.001)
-        self.rst(0)
-        time.sleep(0.01)
-        self.rst(1)
-        
-        self.write_cmd(0xAE)   #turn off OLED display
+# Use for SPI
+spi = board.SPI()
+oled_cs = digitalio.DigitalInOut(board.D5)
+oled_dc = digitalio.DigitalInOut(board.D6)
+oled = adafruit_ssd1306.SSD1306_SPI(WIDTH, HEIGHT, spi, oled_dc, oled_reset, oled_cs)
 
-        self.write_cmd(0x00)   #set lower column address
-        self.write_cmd(0x10)   #set higher column address 
+# Clear display.
+oled.fill(0)
+oled.show()
 
-        self.write_cmd(0xB0)   #set page address 
-      
-        self.write_cmd(0xdc)    #et display start line 
-        self.write_cmd(0x00) 
-        self.write_cmd(0x81)    #contract control 
-        self.write_cmd(0x6f)    #128
-        self.write_cmd(0x21)    # Set Memory addressing mode (0x20/0x21) #
-    
-        self.write_cmd(0xa0)    #set segment remap 
-        self.write_cmd(0xc0)    #Com scan direction
-        self.write_cmd(0xa4)   #Disable Entire Display On (0xA4/0xA5) 
+# Create blank image for drawing.
+# Make sure to create image with mode '1' for 1-bit color.
+image = Image.new("1", (oled.width, oled.height))
 
-        self.write_cmd(0xa6)    #normal / reverse
-        self.write_cmd(0xa8)    #multiplex ratio 
-        self.write_cmd(0x3f)    #duty = 1/64
-  
-        self.write_cmd(0xd3)    #set display offset 
-        self.write_cmd(0x60)
+# Get drawing object to draw on image.
+draw = ImageDraw.Draw(image)
 
-        self.write_cmd(0xd5)    #set osc division 
-        self.write_cmd(0x41)
-    
-        self.write_cmd(0xd9)    #set pre-charge period
-        self.write_cmd(0x22)   
+# Draw a white background
+draw.rectangle((0, 0, oled.width, oled.height), outline=255, fill=255)
 
-        self.write_cmd(0xdb)    #set vcomh 
-        self.write_cmd(0x35)  
-    
-        self.write_cmd(0xad)    #set charge pump enable 
-        self.write_cmd(0x8a)    #Set DC-DC enable (a=0:disable; a=1:enable)
-        self.write_cmd(0XAF)
-    def show(self):
-        self.write_cmd(0xb0)
-        for page in range(0,64):
-            self.column = 63 - page              
-            self.write_cmd(0x00 + (self.column & 0x0f))
-            self.write_cmd(0x10 + (self.column >> 4))
-            for num in range(0,16):
-                self.write_data(self.buffer[page*16+num])
-        
-          
-if __name__=='__main__':
+# Draw a smaller inner rectangle
+draw.rectangle(
+    (BORDER, BORDER, oled.width - BORDER - 1, oled.height - BORDER - 1),
+    outline=0,
+    fill=0,
+)
 
-    OLED = OLED_1inch3()
-    OLED.fill(0x0000) 
-    OLED.show()
-    OLED.rect(0,0,128,64,OLED.white)
-    time.sleep(0.5)
-    OLED.show()
-    OLED.rect(10,22,20,20,OLED.white)
-    time.sleep(0.5)
-    OLED.show()
-    OLED.fill_rect(40,22,20,20,OLED.white)
-    time.sleep(0.5)
-    OLED.show()
-    OLED.rect(70,22,20,20,OLED.white)
-    time.sleep(0.5)
-    OLED.show()
-    OLED.fill_rect(100,22,20,20,OLED.white)
-    time.sleep(0.5)
-    OLED.show()
-    time.sleep(1)
-    
-    OLED.fill(0x0000)
-    OLED.line(0,0,5,64,OLED.white)
-    OLED.show()
-    time.sleep(0.01)
-    OLED.line(0,0,20,64,OLED.white)
-    OLED.show()
-    time.sleep(0.01)
-    OLED.line(0,0,35,64,OLED.white)
-    OLED.show()
-    time.sleep(0.01)
-    OLED.line(0,0,65,64,OLED.white)
-    OLED.show()
-    time.sleep(0.01)
-    OLED.line(0,0,95,64,OLED.white)
-    OLED.show()
-    time.sleep(0.01)
-    OLED.line(0,0,125,64,OLED.white)
-    OLED.show()
-    time.sleep(0.01)
-    OLED.line(0,0,125,41,OLED.white)
-    OLED.show()
-    time.sleep(0.1)
-    OLED.line(0,0,125,21,OLED.white)
-    OLED.show()
-    time.sleep(0.01)
-    OLED.line(0,0,125,3,OLED.white)
-    OLED.show()
-    time.sleep(0.01)
-    
-    OLED.line(127,1,125,64,OLED.white)
-    OLED.show()
-    time.sleep(0.01)
-    OLED.line(127,1,110,64,OLED.white)
-    OLED.show()
-    time.sleep(0.01)
-    OLED.line(127,1,95,64,OLED.white)
-    OLED.show()
-    time.sleep(0.01)
-    OLED.line(127,1,65,64,OLED.white)
-    OLED.show()
-    time.sleep(0.01)
-    OLED.line(127,1,35,64,OLED.white)
-    OLED.show()
-    time.sleep(0.01)
-    OLED.line(127,1,1,64,OLED.white)
-    OLED.show()
-    time.sleep(0.01)
-    OLED.line(127,1,1,44,OLED.white)
-    OLED.show()
-    time.sleep(0.01)
-    OLED.line(127,1,1,24,OLED.white)
-    OLED.show()
-    time.sleep(0.01)
-    OLED.line(127,1,1,3,OLED.white)
-    OLED.show()
-    time.sleep(1)
-    OLED.fill(0x0000) 
-    OLED.text("128 x 64 Pixels",1,10,OLED.white)
-    OLED.text("Pico-OLED-1.3",1,27,OLED.white)
-    OLED.text("SH1107",1,44,OLED.white)  
-    OLED.show()
-    
-    time.sleep(1)
-#    OLED.fill(0x0000) 
-#    keyA = Pin(15,Pin.IN,Pin.PULL_UP)
-#    keyB = Pin(17,Pin.IN,Pin.PULL_UP)
-#    while(1):
-#        if keyA.value() == 0:
-#            OLED.fill_rect(0,0,128,20,OLED.white)
-#            print("A")
-#       else :
-#            OLED.fill_rect(0,0,128,20,OLED.balck)
-            
-            
-#        if(keyB.value() == 0):
-#            OLED.fill_rect(0,44,128,20,OLED.white)
-#            print("B")
-#        else :
-#            OLED.fill_rect(0,44,128,20,OLED.balck)
-#        OLED.fill_rect(0,22,128,20,OLED.white)
-#        OLED.text("press the button",0,28,OLED.balck)
-            
-#        OLED.show()
-    
-    
-    time.sleep(1)
-    OLED.fill(0xFFFF)
+# Load default font.
+font = ImageFont.load_default()
 
+# Draw Some Text
+text = "Hello World!"
+(font_width, font_height) = font.getsize(text)
+draw.text(
+    (oled.width // 2 - font_width // 2, oled.height // 2 - font_height // 2),
+    text,
+    font=font,
+    fill=255,
+)
 
-
-
-
+# Display image
+oled.image(image)
+oled.show()
