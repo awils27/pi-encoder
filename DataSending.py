581a68753a64821f5301830b1b131c3fe59a1a8b
Aiden <68633820+awils27@users.noreply.github.com>
2023-01-11 21:30:12 +1000

    Removed multiply.

--- DataSending.py
+++ DataSending.py
@@ -16,7 +16,7 @@ class SendData(Thread):
 
         while True:
 
-            struct.focus = abs(0.25*Encoder.counter)
+            struct.focus = abs(Encoder.counter)
             bits = struct.createFreeD().encode()
 
 
