41df933cf0533d1d0c62416637f27f42070a0401
Aiden <68633820+awils27@users.noreply.github.com>
2023-01-12 21:10:33 +1000

    Update OLed.py

--- OLed.py
+++ OLed.py
@@ -6,11 +6,10 @@ from luma.core import cmdline
 # rev.1 users set port=0
 # substitute spi(device=0, port=0) below if using that interface
 # substitute bitbang_6800(RS=7, E=8, PINS=[25,24,23,27]) below if using that interface
-serial = gpio_cs_spi(device=0, port=0, gpio_CS=5)
+serial = gpio_cs_spi(device=0, port=0, gpio_CS=5 gpio_DC=6, gpio_RS=4)
 
 # substitute ssd1331(...) or sh1106(...) below if using that device
-params = dict(vars("--gpio-data-command 6 --gpio-reset 4"))
-device = sh1106(serial, params)
+device = sh1106(serial)
 
 with canvas(device) as draw:
     draw.rectangle(device.bounding_box, outline="white", fill="black")
