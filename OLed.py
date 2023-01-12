c62ed529228fb9f8e3fdfbe118b15c267685bda5
Aiden <68633820+awils27@users.noreply.github.com>
2023-01-12 20:49:49 +1000

    Update OLed.py

--- OLed.py
+++ OLed.py
@@ -6,7 +6,7 @@ from luma.core import cmdline
 # rev.1 users set port=0
 # substitute spi(device=0, port=0) below if using that interface
 # substitute bitbang_6800(RS=7, E=8, PINS=[25,24,23,27]) below if using that interface
-serial = gpio_cs_spi(device=0, port=0)
+serial = gpio_cs_spi(device=0, port=0, gpio_CS=5)
 
 # substitute ssd1331(...) or sh1106(...) below if using that device
 device = sh1106(serial)
