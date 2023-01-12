3ff355d8bc6423cb3dd3e2edcb585587cdf59912
Aiden <68633820+awils27@users.noreply.github.com>
2023-01-12 21:57:18 +1000

    Update OledSetup.py

--- OledSetup.py
+++ OledSetup.py
@@ -11,19 +11,11 @@ def get_device(actual_args):
     """
     Create device from command-line arguments and return it.
     """
-    if actual_args is None:
-        actual_args = sys.argv[1:]
     parser = cmdline.create_parser(description='luma.examples arguments')
     args = parser.parse_args(actual_args)
 
-    if args.config:
-        # load config from file
-        config = cmdline.load_config(args.config)
-        args = parser.parse_args(config + actual_args)
-
     # create device
     try:
-        print (args)
         device = cmdline.create_device(args)
         return device
 
