1182846f0e57918af24f23c7868de25986c74b16
Aiden <68633820+awils27@users.noreply.github.com>
2023-01-12 22:05:43 +1000

    Update OLed.py

--- OLed.py
+++ OLed.py
@@ -6,6 +6,8 @@ from luma.core.render import canvas
 
 arguments = ['--display', 'sh1106', '--interface', 'gpio_cs_spi', '--gpio-data-command', '24', '--gpio-chip-select', '8', '--gpio-reset', '25', '--rotate', '2']
 
+device = get_device(arguments)
+
 print("Testing screen updates...")
 time.sleep(2)
 for x in range(40):
