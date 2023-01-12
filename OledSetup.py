cd4efefa644eda6b5748506b6cb100646f9cbbb4
Aiden <68633820+awils27@users.noreply.github.com>
2023-01-12 21:49:11 +1000

    Update OledSetup.py

--- OledSetup.py
+++ OledSetup.py
@@ -8,36 +8,6 @@ import logging
 from luma.core import cmdline, error
 
 
-# logging
-logging.basicConfig(
-    level=logging.DEBUG,
-    format='%(asctime)-15s - %(message)s'
-)
-# ignore PIL debug messages
-logging.getLogger('PIL').setLevel(logging.ERROR)
-
-
-def display_settings(device, args):
-    """
-    Display a short summary of the settings.
-    :rtype: str
-    """
-    iface = ''
-    display_types = cmdline.get_display_types()
-    if args.display not in display_types['emulator']:
-        iface = f'Interface: {args.interface}\n'
-
-    lib_name = cmdline.get_library_for_display_type(args.display)
-    if lib_name is not None:
-        lib_version = cmdline.get_library_version(lib_name)
-    else:
-        lib_name = lib_version = 'unknown'
-
-    import luma.core
-    version = f'luma.{lib_name} {lib_version} (luma.core {luma.core.__version__})'
-
-    return f'Version: {version}\nDisplay: {args.display}\n{iface}Dimensions: {device.width} x {device.height}\n{"-" * 60}'
-
 
 def get_device(actual_args=None):
     """
