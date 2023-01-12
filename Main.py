4801ab94b6165fb49bd8f22d12414d942a7b3290
Aiden <68633820+awils27@users.noreply.github.com>
2023-01-12 22:41:31 +1000

    Integrating oled into main code

--- Main.py
+++ Main.py
@@ -3,6 +3,7 @@ import Button
 import Encoder
 import DataSending
 import atexit
+import Oled
 from RPi import GPIO
 
 
@@ -11,6 +12,13 @@ EncoderA = 21
 EncoderB = 20
 UDP_IP = "192.168.1.68"
 UDP_PORT = 4000
+Display = "sh1106"
+Interface = "gpio_cs_spi"
+DC_Pin = "24"
+CS_Pin = "8"
+RST_Pin = "25"
+ScreenRot = "2"
+Encoder_Frq = 60
 
 
 
@@ -25,10 +33,16 @@ def button_pressed_callback(channel):
 atexit.register(ExitHandler)
 
 Encoder.SetupEncoders(EncoderA, EncoderB)
+
+Display = Oled.IntialiseDisplay(Display, Interface, DC_Pin, CS_Pin, RST_Pin, ScreenRot)
+
+
+
 T1Encoders = Thread(target = Encoder.EncoderThread.ReadEncoderValues, args = (EncoderA, EncoderB))
 
 T2Data = Thread(target = DataSending.SendData.SendFreeDData, args = (UDP_IP, UDP_PORT))
 
+T3Screen = Thread(target = Oled.OLEDScreen.StartOLEDDisplay, args = (UDP_IP, UDP_PORT, Encoder_Frq, Display))
 
 T1Encoders.start()
 T2Data.start()
--- OLed.py
+++ OLed.py
@@ -2,23 +2,33 @@ import time
 from OledSetup import get_device
 from luma.core.render import canvas
 import Encoder
-import Main
+from threading import Thread
 
-arguments = ['--display', 'sh1106', '--interface', 'gpio_cs_spi', '--gpio-data-command', '24', '--gpio-chip-select', '8', '--gpio-reset', '25', '--rotate', '2']
+#['--display', 'sh1106', '--interface', 'gpio_cs_spi', '--gpio-data-command', '24', '--gpio-chip-select', '8', '--gpio-reset', '25', '--rotate', '2']
 
-device = get_device(arguments)
+def IntialiseDisplay(display, interface, DC, CS, RST, Rot):
 
-print("Testing screen updates...")
-while True:
-    for x in range(40):
-        if x < 20:
-            text = Main.UDP_IP + ":" + str(Main.UDP_PORT)
-        else:
-            text = "60hz Genlock"
 
-        with canvas(device) as draw:
-            draw.text((5, 2), "Lens Encoder V15.04", fill="white")
-            draw.text((5, 15), "Zoom  " + str(abs(Encoder.counter)), fill="white")
-            draw.text((5, 30), "Focus  1000", fill="white")
-            draw.text((5, 45), text, fill="white")
-            time.sleep(0.1)
+    arguments = ['--display', display, '--interface', interface, '--gpio-data-command', DC, '--gpio-chip-select', CS, '--gpio-reset', RST, '--rotate', Rot]
+    device = get_device(arguments)
+
+    print("Starting Display")
+
+    return device
+
+
+class OLEDScreen(Thread):
+    def StartOLEDDisplay(IpAddr, Port, Freq, device):
+        while True:
+            for x in range(40):
+                if x < 20:
+                    text = IpAddr + ":" + str(Port)
+                else:
+                    text = str(Freq) + "hz Genlock"
+
+                with canvas(device) as draw:
+                    draw.text((5, 2), "Lens Encoder V15.04", fill="white")
+                    draw.text((5, 15), "Zoom  " + str(abs(Encoder.counter)), fill="white")
+                    draw.text((5, 30), "Focus  1000", fill="white")
+                    draw.text((5, 45), text, fill="white")
+                    time.sleep(0.1)
