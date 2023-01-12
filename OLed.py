d1d3df835567343fdbd8659d00c458fa96e3dea9
Aiden <68633820+awils27@users.noreply.github.com>
2023-01-12 21:26:10 +1000

    Update OLed.py

--- OLed.py
+++ OLed.py
@@ -1,15 +1,39 @@
 from luma.core.interface.serial import gpio_cs_spi
 from luma.core.render import canvas
 from luma.oled.device import sh1106
-from luma.core import cmdline
 
-# rev.1 users set port=0
-# substitute spi(device=0, port=0) below if using that interface
-# substitute bitbang_6800(RS=7, E=8, PINS=[25,24,23,27]) below if using that interface
-serial = gpio_cs_spi(device=0, port=0)
 
-# substitute ssd1331(...) or sh1106(...) below if using that device
-device = sh1106(serial)
+import sys
+import logging
+
+from luma.core import cmdline, error
+
+
+def get_device(actual_args=None):
+    """
+    Create device from command-line arguments and return it.
+    """
+    if actual_args is None:
+        actual_args = sys.argv[1:]
+    parser = cmdline.create_parser(description='luma.examples arguments')
+    args = parser.parse_args(actual_args)
+
+    if args.config:
+        # load config from file
+        config = cmdline.load_config(args.config)
+        args = parser.parse_args(config + actual_args)
+
+    # create device
+    try:
+        device = cmdline.create_device(args)
+        return device
+
+    except error.Error as e:
+        parser.error(e)
+        return None
+
+device = get_device("--display sh1106 --interface gpio_cs_spi --gpio-data-command 6  --gpio-chip-select 5 --gpio-reset 4 --rotate 2")
+
 
 with canvas(device) as draw:
     draw.rectangle(device.bounding_box, outline="white", fill="black")
